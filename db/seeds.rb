fbd6130f30dc316cbf748c8fd335750c94dd32f9
Jason <jas0nle@yahoo.com>
2019-11-13 09:16:03 -0600

    Changed a seed room playlist

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,12 @@ just_good_music = RSpotify::Playlist.find('spotify', "37i9dQZF1DX0b1hHYQtJjp")
 get_turnt = RSpotify::Playlist.find('spotify', "37i9dQZF1DWY4xHQp97fN6")
 hot_country = RSpotify::Playlist.find('spotify', "37i9dQZF1DX1lVhptIYRda")
 workout = RSpotify::Playlist.find('spotify', "37i9dQZF1DX70RN3TfWWJh")
-morning_classical = RSpotify::Playlist.find('spotify', "37i9dQZF1DX9OZisIoJQhG")
+top_hits = RSpotify::Playlist.find('spotify', "37i9dQZF1DXcBWIGoYBM5M")
 
 Room.create(playlist_name: "Just Good Music", playlist_id: "37i9dQZF1DX0b1hHYQtJjp", playlist_image_url: just_good_music.images[0]['url'], playlist_description: just_good_music.description)
 Room.create(playlist_name: "Get Turnt", playlist_id: "37i9dQZF1DWY4xHQp97fN6", playlist_image_url: get_turnt.images[0]['url'], playlist_description: get_turnt.description)
 Room.create(playlist_name: "Hot Country", playlist_id: "37i9dQZF1DX1lVhptIYRda", playlist_image_url: hot_country.images[0]['url'], playlist_description: hot_country.description)
 Room.create(playlist_name: "Workout", playlist_id: "37i9dQZF1DX70RN3TfWWJh", playlist_image_url: workout.images[0]['url'], playlist_description: workout.description)
-Room.create(playlist_name: "Morning Classical", playlist_id: "37i9dQZF1DX9OZisIoJQhG", playlist_image_url: morning_classical.images[0]['url'], playlist_description: morning_classical.description)
+Room.create(playlist_name: "Today's Top Hits", playlist_id: "37i9dQZF1DX9OZisIoJQhG", playlist_image_url: morning_classical.images[0]['url'], playlist_description: morning_classical.description)
 
 
